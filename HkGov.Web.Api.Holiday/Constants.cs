f442f0959a55f5371a3dfc84160c2238e1494eeb
ChunHo.LING <ChunHo.LING@IT-AP-PC>
2019-01-23 15:00:23 +0800

    Add project files.

---
+++ HkGov.Web.Api.Holiday/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HkGov.Web.Api.Holiday
+{
+    public class Constants
+    {
+        public class Api
+        {
+            public const string Title = "HKGov Public Holiday";
+            public const string Version = "v1";
+            public const string SwaggerEndpoint = "/swagger/v1/swagger.json";
+        }
+    }
+}
---
+++ HkGov.Web.Api.Holiday/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace HkGov.Web.Api.Holiday
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(Constants.Api.Version, new Info { Title = Constants.Api.Title, Version = Constants.Api.Version });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                string endpointName = Constants.Api.Title + " " + Constants.Api.Version;
+                c.SwaggerEndpoint(Constants.Api.SwaggerEndpoint, endpointName);
+            });
+
+            app.UseMvc();
+        }
+    }
+}
